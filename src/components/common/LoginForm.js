5ff1a0dc58a4aa555108ddb583286c2fa0919d09
Kiril Grischenko <grischenkokiril@gmail.com>
2019-01-19 14:41:09 +0200

    Set up signin form by Formik

--- src/components/common/LoginForm.js
+++ src/components/common/LoginForm.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import styled from 'styled-components';
+import { position, rgba } from 'polished';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faGoogle, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { map, transform } from 'lodash';
+
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import InputAdornment from '@material-ui/core/InputAdornment';
@@ -12,11 +21,6 @@ import AccountCircleIcon from '@material-ui/icons/AccountCircleRounded';
 import VisibilityIcon from '@material-ui/icons/VisibilityRounded';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOffRounded';
 import Button from '@material-ui/core/Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faGoogle, faGithub } from '@fortawesome/free-brands-svg-icons';
-import styled from 'styled-components';
-import { map } from 'lodash';
-import { position, rgba } from 'polished';
 
 import { getStyledProps, getPadding } from '../../styles';
 
@@ -62,7 +66,7 @@ const SignUpButton = styled(Button)`
   }
 `;
 
-const BrandIconsWrapper = styled.div`
+const BrandIconContainer = styled.div`
   width: 100%;
   height: 62px;
   display: flex;
@@ -71,69 +75,106 @@ const BrandIconsWrapper = styled.div`
   margin-top: 3em;
 `;
 
-const BrandIconElement = styled.div`
+const BrandIconButton = styled(Fab)`
+  && {
+    color: #fff;
+  }
+`;
+
+const BrandIcon = styled.div`
   display: inline-flex;
 
   :not(:last-child) {
     margin-right: 2em;
   }
 
-  :nth-child(2) {
-    align-self: flex-end;
-  }
-
-  button {
-    color: #fff;
+  ${BrandIconButton} {
     background-color: ${({ brandColor }) => brandColor};
 
     :hover {
-      background-color: ${({ brandColor }) => rgba(brandColor, 0.7)};
+      background-color: ${({ brandColor }) => rgba(brandColor, 0.8)};
     }
   }
 `;
 
-const formFields = {
-  login: [
-    {
-      id: 'username',
-      label: 'Username',
-      type: 'text',
-      placeholder: 'Username or email',
-      autocomplete: 'on',
-      required: true,
-    },
-    {
-      id: 'password',
-      label: 'Password',
-      type: 'password',
-      placeholder: '******',
-      autocomplete: 'on',
-      required: true,
-    },
-  ],
+const formConfig = {
+  signin: [{
+    name: 'username',
+    label: 'Username',
+    type: 'text',
+    placeholder: 'username or email',
+    autocomplete: 'on',
+    required: true,
+    initial: '',
+  }, {
+    name: 'password',
+    label: 'Password',
+    type: 'password',
+    placeholder: '******',
+    autocomplete: 'on',
+    required: true,
+    initial: '',
+  }],
 };
+
+const formValidationSchemas = {
+  signin: yup.object().shape(transform(formConfig.signin, (res, { name }) => {
+    // eslint-disable-next-line
+    switch (name) {
+      case 'username': {
+        res[name] = yup.string()
+          .min(2)
+          .max(20)
+          .required('*required');
+        break;
+      }
+      case 'password': {
+        res[name] = yup.string()
+          .min(6)
+          .max(20)
+          .required('*required');
+        break;
+      }
+    }
+  }), {}),
+};
+
+const formInitialValues = transform(formConfig, (form, val, key) => {
+  form[key] = transform(val, (field, { name, initial }) => {
+    field[name] = initial;
+  }, {});
+}, {});
 
 const brandIcons = [
   {
     id: 'facebook',
-    color: 'blue',
+    color: '#3232ff',
     iconElement: faFacebook,
   },
   {
     id: 'google',
-    color: 'red',
+    color: '#ff3232',
     iconElement: faGoogle,
   },
   {
     id: 'github',
-    color: 'black',
+    color: '#000',
     iconElement: faGithub,
   },
 ];
 
 class LoginForm extends Component {
-  state = {
-    showPassword: false,
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showPassword: false,
+    };
+
+    this.formId = 'signin';
+    this.initialValues = formInitialValues[this.formId];
+    this.formFields = formConfig[this.formId];
+    this.formValidationSchema = formValidationSchemas[this.formId];
   }
 
   toggleShowPassword = () => {
@@ -144,7 +185,7 @@ class LoginForm extends Component {
     const { showPassword } = this.state;
 
     return (
-      <Wrapper brandColor="piska" elevation={8}>
+      <Wrapper elevation={8}>
         <Title
           color="primary"
           variant="h3"
@@ -156,89 +197,114 @@ class LoginForm extends Component {
           </AccountIconWrapper>
           {'Sign In'}
         </Title>
-        <Form onSubmit={(event) => { event.preventDefault(); }}>
-          {
-            map(formFields.login, ({
-              id,
-              label,
-              type,
-              placeholder,
-              autocomplete,
-              required,
-            }) => {
-              const isPasswordField = type === 'password';
-              const verifiedType = (isPasswordField && showPassword)
-                ? 'text'
-                : type;
+        <Formik
+          initialValues={this.initialValues}
+          validationSchema={this.formValidationSchema}
+          onSubmit={(values, actions) => {
+            console.log(values, actions);
+          }}
+          render={({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+          }) => {
+            return (
+              <Form onSubmit={(event) => { event.preventDefault(); }}>
+                {
+                  map(this.formFields, ({
+                    name,
+                    label,
+                    type,
+                    placeholder,
+                    autocomplete,
+                    required,
+                  }) => {
+                    const isError = errors[name] && touched[name];
+                    const isPasswordField = type === 'password';
+                    const error = errors[name];
+                    const verifiedType = (isPasswordField && showPassword)
+                      ? 'text'
+                      : type;
 
-              return (
-                <TextField
-                  key={id}
-                  required={required}
+                    return (
+                      <TextField key={name} fullWidth>
+                        <InputLabel htmlFor={name}>{label}</InputLabel>
+                        <Input
+                          id={name}
+                          type={verifiedType}
+                          value={values[name]}
+                          error={errors[name] && touched[name]}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          placeholder={placeholder}
+                          required={required}
+                          autoComplete={autocomplete}
+                          endAdornment={(
+                            <Choose>
+                              <When condition={isPasswordField}>
+                                <InputAdornment position="end">
+                                  <IconButton onClick={this.toggleShowPassword}>
+                                    <Choose>
+                                      <When condition={showPassword}>
+                                        <VisibilityIcon />
+                                      </When>
+                                      <Otherwise>
+                                        <VisibilityOffIcon />
+                                      </Otherwise>
+                                    </Choose>
+                                  </IconButton>
+                                </InputAdornment>
+                              </When>
+                              <Otherwise>{null}</Otherwise>
+                            </Choose>
+                          )}
+                        />
+                        <If condition={isError}>
+                          <FormHelperText error={isError}>{error}</FormHelperText>
+                        </If>
+                      </TextField>
+                    );
+                  })
+                }
+                <SubmitButton
+                  color="primary"
+                  size="large"
+                  variant="contained"
+                  onClick={handleSubmit}
                   fullWidth
                 >
-                  <InputLabel htmlFor={id}>{label}</InputLabel>
-                  <Input
-                    id={id}
-                    type={verifiedType}
-                    placeholder={placeholder}
-                    autoComplete={autocomplete}
-                    endAdornment={(
-                      <Choose>
-                        <When condition={isPasswordField}>
-                          <InputAdornment position="end">
-                            <IconButton onClick={this.toggleShowPassword}>
-                              <Choose>
-                                <When condition={showPassword}>
-                                  <VisibilityIcon />
-                                </When>
-                                <Otherwise>
-                                  <VisibilityOffIcon />
-                                </Otherwise>
-                              </Choose>
-                            </IconButton>
-                          </InputAdornment>
-                        </When>
-                        <Otherwise>{null}</Otherwise>
-                      </Choose>
-                    )}
-                  />
-                </TextField>
-              );
-            })
-          }
-          <SubmitButton
-            color="primary"
-            size="large"
-            variant="contained"
-            fullWidth
-          >
-            {'Sign In'}
-          </SubmitButton>
-          <SignUpButton
-            color="primary"
-            size="large"
-            variant="outlined"
-            fullWidth
-          >
-            {'Sign Up'}
-          </SignUpButton>
-          <BrandIconsWrapper>
-            {
-              map(brandIcons, ({ id, color, iconElement }) => (
-                <BrandIconElement
-                  key={id}
-                  brand={id}
-                  brandColor={color}
+                  {'Sign In'}
+                </SubmitButton>
+                <SignUpButton
+                  color="primary"
+                  size="large"
+                  variant="outlined"
+                  fullWidth
                 >
-                  <Fab size="large">
-                    <FontAwesomeIcon icon={iconElement} size="lg" />
-                  </Fab>
-                </BrandIconElement>
-              ))
-            }
-          </BrandIconsWrapper>
-        </Form>
+                  {'Sign Up'}
+                </SignUpButton>
+                <BrandIconContainer>
+                  {
+                    map(brandIcons, ({ id, color, iconElement }) => (
+                      <BrandIcon
+                        key={id}
+                        brand={id}
+                        brandColor={color}
+                      >
+                        <BrandIconButton size="large">
+                          <FontAwesomeIcon icon={iconElement} size="lg" />
+                        </BrandIconButton>
+                      </BrandIcon>
+                    ))
+                  }
+                </BrandIconContainer>
+              </Form>
+            );
+          }}
+        />
       </Wrapper>
     );
   }

a767e9b8bde1d398760e162f0a9db0989467dd28
Kirill Grischenko <grischenkokiril@gmail.com>
2019-10-17 16:23:53 +0300

    dev

--- scripts/docker-server-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp -r /graphichat-server/cache_modules/node_modules/. /graphichat-server/server/node_modules/
-exec yarn run dev
---
+++ server/.eslintrc.js
@@ -0,0 +1,11 @@
+ module.exports = {
+  "env": {
+    "es6": true,
+    "node": true,
+    "mongo": true,
+  },
+  "extends": [
+    "airbnb-base/legacy",
+  ],
+  "parser": "babel-eslint",
+};

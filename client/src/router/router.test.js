1459167abec636375074c21929464fc50944b11d
Kirill Grischenko <grischenkokiril@gmail.com>
2019-12-06 10:24:00 +0200

    update tests for AppRoute component

--- client/src/router/router.test.js
+++ client/src/router/router.test.js
@@ -14,63 +14,53 @@ const testLayout = ({ children }) => (
   <div>{children}</div>
 );
 
+const AppRouteComponent = props => (
+  <AppRoute path="/" layout={testLayout} component={testComponent} {...props} />
+);
+
+const mountRouter = (routes, initialEntries = ['/']) => mount((
+  <MemoryRouter initialEntries={initialEntries}>
+    {routes}
+  </MemoryRouter>
+));
+
 describe('test router', () => {
   describe('test AppRoute', () => {
     test('snapshot render', () => {
-      const component = shallow(<AppRoute component={testComponent} />);
+      const component = shallow(<AppRoute path="/" layout={testLayout} component={testComponent} />);
 
       expect(component).toMatchSnapshot();
     });
 
-    test('Route render', () => {
-      const routerWrapper = mount((
-        <MemoryRouter initialEntries={['/']}>
-          <AppRoute path="/" layout={testLayout} component={testComponent} />
-        </MemoryRouter>
-      ));
+    test('Route render is defined', () => {
+      const routerWrapper = mountRouter(<AppRouteComponent />);
 
       expect(routerWrapper.find(Route).exists()).toEqual(true);
       expect(routerWrapper.find(Route).prop('render')).toBeDefined();
     });
 
     test('pass props to Route', () => {
-      const routerWrapper = mount((
-        <MemoryRouter initialEntries={['/']}>
-          <AppRoute path="/" layout={testLayout} component={testComponent} testProps="testProps" />
-        </MemoryRouter>
-      ));
+      const routerWrapper = mountRouter(<AppRouteComponent testProps="testProps" />);
 
       expect(routerWrapper.find(Route).prop('testProps')).toEqual('testProps');
     });
 
     test('redirect render', () => {
-      const routerWrapper = mount((
-        <MemoryRouter initialEntries={['/']}>
-          <AppRoute path="/" layout={testLayout} component={testComponent} redirect />
-        </MemoryRouter>
-      ));
+      const routerWrapper = mountRouter(<AppRouteComponent redirect />);
 
       expect(routerWrapper.find(testComponent).exists()).toEqual(true);
       expect(routerWrapper.find(testLayout).exists()).toEqual(false);
     });
 
     test('layout render', () => {
-      const routerWrapper = mount((
-        <MemoryRouter initialEntries={['/']}>
-          <AppRoute path="/" layout={testLayout} component={testComponent} />
-        </MemoryRouter>
-      ));
+      const routerWrapper = mountRouter(<AppRouteComponent />);
 
       expect(routerWrapper.find(testComponent).exists()).toEqual(true);
       expect(routerWrapper.find(testLayout).exists()).toEqual(true);
     });
 
     test('privateRoute render', () => {
-      const routerWrapper = mount((
-        <MemoryRouter initialEntries={['/']}>
-          <AppRoute path="/" layout={testLayout} component={testComponent} privateRoute />
-        </MemoryRouter>
-      ));
+      const routerWrapper = mountRouter(<AppRouteComponent privateRoute />);
 
       expect(routerWrapper.find(PrivateRoute).exists()).toEqual(true);
     });

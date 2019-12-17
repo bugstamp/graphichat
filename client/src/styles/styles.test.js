d53a5f1fb217339fb1dddc9cff4df31e02c68cb2
Kirill Grischenko <grischenkokiril@gmail.com>
2019-12-17 16:31:02 +0200

    Fixed styles tests

--- client/src/styles/styles.test.js
+++ client/src/styles/styles.test.js
@@ -3,28 +3,37 @@ import {
   getSpacing,
 } from './index';
 
-const mockStyledProps = {
-  theme: {
-    border: 1,
-    spacing: num => num * 8,
-    palette: {
-      primary: {
-        light: '#fff',
-      },
+const mockedTheme = {
+  border: 1,
+  spacing: num => num * 8,
+  palette: {
+    primary: {
+      light: '#fff',
     },
   },
 };
+const mockStyledProps = {
+  theme: mockedTheme,
+};
 
 describe('test styles utils', () => {
-  test('test getStyledProps', () => {
-    expect(getStyledProps()()).toBeNull();
-    expect(getStyledProps('', 'px')(mockStyledProps)).toBeNull();
-    expect(getStyledProps('theme.palette.primary.light')(mockStyledProps)).toEqual('#fff');
+  describe('test getStyledProps', () => {
+    test('if not pass styled props', () => {
+      expect(getStyledProps()()).toBeNull();
+    });
+    test('getting existing value', () => {
+      expect(getStyledProps('theme.palette.primary.light')(mockStyledProps)).toEqual('#fff');
+    });
+    test('getting existing value with unit', () => {});
     expect(getStyledProps('theme.border', 'px')(mockStyledProps)).toEqual('1px');
   });
 
-  test('test getSpacing', () => {
-    expect(getSpacing(1)(mockStyledProps)).toEqual('8px');
-    expect(getSpacing(1, 'em')(mockStyledProps)).toEqual('8em');
+  describe('test getSpacing', () => {
+    test('getting spacing', () => {
+      expect(getSpacing(1)(mockStyledProps)).toEqual('8px');
+    });
+    test('getting spacing with unit', () => {
+      expect(getSpacing(1, 'em')(mockStyledProps)).toEqual('8em');
+    });
   });
 });

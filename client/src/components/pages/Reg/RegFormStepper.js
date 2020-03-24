449496f6ba74a71378c35ea82eae9252ce527457
Kiril Grischenko <grischenkokiril@gmail.com>
2020-03-24 18:42:56 +0200

    RegFormStepper tests

--- client/src/components/pages/Reg/RegFormStepper.js
+++ client/src/components/pages/Reg/RegFormStepper.js
@@ -6,54 +6,53 @@ import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 
 import StepLabelIcon from './StepLabelIcon';
-
 import { StepLabelStyled as StepLabel } from './styled';
 
-const RegFormStepper = ({ activeStep }) => {
-  const steps = [
-    'Create your account',
-    'Tell us about yourself',
-  ];
+export const steps = [
+  'Create your account',
+  'Tell us about yourself',
+];
 
-  return (
-    <Stepper alternativeLabel activeStep={activeStep}>
-      {
-        map(steps, (label, index) => {
-          const isActive = activeStep === index;
-          const isCompleted = index < activeStep;
-          const iconColor = (!isCompleted && !isActive)
-            ? 'disabled'
-            : 'primary';
-          const step = index + 1;
+const RegFormStepper = ({ activeStep }) => (
+  <Stepper alternativeLabel activeStep={activeStep}>
+    {
+      map(steps, (label, index) => {
+        const step = index + 1;
+        const isActive = activeStep === index;
+        const isCompleted = index < activeStep;
+        const iconColor = (!isCompleted && !isActive)
+          ? 'disabled'
+          : 'primary';
 
-          return (
-            <Step
-              key={index}
-              active={isActive}
-              completed={isCompleted}
+        return (
+          <Step
+            key={index}
+            active={isActive}
+            completed={isCompleted}
+          >
+            <StepLabel
+              icon={(
+                <StepLabelIcon
+                  isCompleted={isCompleted}
+                  step={step}
+                  iconColor={iconColor}
+                />
+              )}
             >
-              <StepLabel
-                icon={(
-                  <StepLabelIcon
-                    isCompleted={isCompleted}
-                    step={step}
-                    iconColor={iconColor}
-                  />
-                )}
-              >
-                <span>{`Step ${step}.`}</span>
-                <p>{label}</p>
-              </StepLabel>
-            </Step>
-          );
-        })
-      }
-    </Stepper>
-  );
-};
+              <span className="step">{`Step ${step}.`}</span>
+              <p>{label}</p>
+            </StepLabel>
+          </Step>
+        );
+      })
+    }
+  </Stepper>
+);
 
 RegFormStepper.propTypes = {
   activeStep: PropTypes.number.isRequired,
 };
+
+export { RegFormStepper };
 
 export default memo(RegFormStepper);

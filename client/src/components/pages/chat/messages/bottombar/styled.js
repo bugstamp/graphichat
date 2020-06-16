06e8d86ec89d3ace6b800d870502ed34d1760deb
Kiril Grischenko <grischenkokiril@gmail.com>
2020-06-16 18:33:48 +0300

    fix chat bottom bar styles

--- client/src/components/pages/chat/messages/bottombar/styled.js
+++ client/src/components/pages/chat/messages/bottombar/styled.js
@@ -8,6 +8,46 @@ import { ListItemAvatarStyled } from '../../../../common/List/ListItemAvatar';
 
 import { getStyledProps, getSpacing } from '../../../../../styles';
 
+export const FormInput = styled(Input)`
+  && {
+    flex: 1 auto;
+    display: flex;
+    align-items: flex-start;
+  }
+`;
+
+export const Form = styled.form`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  position: relative;
+  padding: ${getSpacing(1)} 0;
+
+  ${(props) => {
+    const breakpoints = getStyledProps('theme.breakpoints')(props);
+    const smDown = breakpoints.down('sm');
+
+    return `
+      ${smDown} {
+        flex-flow: row nowrap;
+        align-items: center;
+        padding: ${getSpacing(0.5)(props)};
+
+        ${FormInput} {
+          padding: ${getSpacing(1)(props)};
+          border: 1px solid ${getStyledProps('theme.palette.grey.400')(props)};
+          border-radius: 20px;
+
+          &:before,
+          &:after {
+            display: none;
+          }
+        }
+      }
+    `;
+  }}
+`;
+
 export const ChatBottomBarStyled = styled.div`
   height: 72px;
   display: flex;
@@ -30,56 +70,6 @@ export const ChatBottomBarStyled = styled.div`
       }
     `;
   }}
-  }
-`;
-
-export const Form = styled.form`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  position: relative;
-  padding: ${getSpacing(1)} 0;
-
-  ${(props) => {
-    const breakpoints = getStyledProps('theme.breakpoints')(props);
-    const smDown = breakpoints.down('sm');
-
-    return `
-      ${smDown} {
-        min-height: auto;
-        flex-flow: row nowrap;
-        align-items: center;
-        padding: ${getSpacing(0.5)(props)};
-      }
-    `;
-  }}
-`;
-
-export const FormInput = styled(Input)`
-  && {
-    flex: 1 auto;
-    display: flex;
-    align-items: flex-start;
-
-  ${(props) => {
-    const breakpoints = getStyledProps('theme.breakpoints')(props);
-    const spacing = getStyledProps('theme.spacing')(props);
-    const smDown = breakpoints.down('sm');
-
-    return `
-      ${smDown} {
-        padding: ${spacing(1)}px;
-        border: 1px solid ${getStyledProps('theme.palette.grey.400')(props)};
-        border-radius: 20px;
-
-        &:before,
-        &:after {
-          display: none;
-        }
-      }
-    `;
-  }}
-  }
 `;
 
 export const SubmitButton = styled(IconButton)`
